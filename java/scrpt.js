daf16750b767da412bc114d9399665a46b9f4de0
guisandodesign <marta.estebanez@alumnos.udit.es>
2026-01-02 19:22:09 +0100

    menú transparente

--- java/scrpt.js
+++ java/scrpt.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
 
     // HAMBURGER
-    $('.hamburger').click(function () {
-        $('.side-menu').css('left', '0');
-        $('.overlay').css({
-            opacity: 1,
-            pointerEvents: 'all'
-        });
+  $('.hamburger').click(function () {
+    $('.side-menu').addClass('open').css('left', '0');
+    $('.overlay').css({
+        opacity: 1,
+        pointerEvents: 'all'
     });
+});
 
-    $('.overlay').click(function () {
-        $('.side-menu').css('left', '-250px');
-        $('.overlay').css({
-            opacity: 0,
-            pointerEvents: 'none'
-        });
+$('.overlay').click(function () {
+    $('.side-menu').removeClass('open').css('left', '-250px');
+    $('.overlay').css({
+        opacity: 0,
+        pointerEvents: 'none'
     });
+});
+
 
     // FLIP CARDS
     $('.card').click(function () {
